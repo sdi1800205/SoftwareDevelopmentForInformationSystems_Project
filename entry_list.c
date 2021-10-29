10f65e4b2ac2383866adf5f68b0690bbd554df09
Greg Galanakis <galanakis.grigoris@gmail.com>
2021-10-29 20:14:29 +0300

    add add_entry()

--- entry_list.c
+++ entry_list.c
@@ -21,7 +21,7 @@ ErrorCode create_entry(const char* w, entry* e) {
     }
 
     //allocate memory for the word
-    newEntry->word = malloc((strlen(w))*sizeof(*w));
+    newEntry->word = malloc((strlen(w)+1)*sizeof(*w));
     if (newEntry->word == NULL){
         printf("Error while allocating memory\n");
         return EC_NO_AVAIL_RES;
@@ -41,6 +41,9 @@ ErrorCode create_entry(const char* w, entry* e) {
 }
 
 ErrorCode destroy_entry(entry *e) {
+
+    free((*e)->word);
+    free((*e)->payload);
     free(*e);
     if (*e != NULL) {
         printf("Given entry was not successully destroyed\n");
@@ -67,6 +70,55 @@ ErrorCode create_entry_list(entry_list* el) {
     newEntryList->dummy = LIST_BOF;
     newEntryList->last = LIST_EOF;
     newEntryList->size = 0;
+
+    return EC_SUCCESS;
+}
+
+unsigned int get_number_entries(const entry_list* el) {
+    if (*el == NULL) {
+        printf("Entry is list not initialized\n");
+        return 0;
+    }
+
+    return (*el)->size;
+}
+
+ErrorCode add_entry(entry_list* el, const entry* e) {
+    if (*el == NULL || *e == NULL) {
+        printf("Given parameters are null\n");
+        return EC_FAIL;
+    }
+
+    // Allocate memory for the new entry
+    entry new_entry = malloc(sizeof(struct entry));
+    if (new_entry == NULL) {
+        printf("Error while allocating memory\n");
+        return EC_FAIL;
+    }
+
+    // Copy the values of given entry to the new entry
+    new_entry->payload = (*e)->payload;
+
+    new_entry->word = malloc(strlen(((*e)->word)+1)*sizeof(char));
+    if (new_entry->word == NULL) {
+        printf("Error while allocating memory\n");
+        return EC_FAIL;
+    }
+    strcpy(new_entry->word, (*e)->word);
+
+    // this entry will be added to the end of list, so it doesn't have next entry
+    new_entry->next = LIST_EOF;
+
+    // Check if this is the first entry of entry_list
+    if((*el)->size == 0) {
+        (*el)->dummy->next = new_entry;
+        (*el)->size++;
+        (*el)->last = new_entry;
+    }
+
+    // set the last entry of entry list as this node
+    (*el)->last->next = new_entry;
+    (*el)->last = new_entry;
 
     return EC_SUCCESS;
 }
