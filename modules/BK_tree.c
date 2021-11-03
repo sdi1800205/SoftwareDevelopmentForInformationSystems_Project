5ffd7ba9a7b65bd21f8d9357d7c4c9ae254acb82
sdi1800205 <sdi1800205@uoa.com>
2021-11-03 15:55:27 +0200

    vag(more BK_tree.c)

--- modules/BK_tree.c
+++ modules/BK_tree.c
@@ -4,14 +4,29 @@
 #include "interface.h"
 #include "BK_tree.h"
 
+// pointer to function that finds the distance of 2 words of 2 entries
+typedef int (*DistanceFunc)(entry* a, entry* b);
+
 struct  BK_tree {
-    BK_node* root;
+    BK_node* root;		// root of tree/index
+	MatchType match_type;		// type that matches words
 };
 
 struct BK_node {
 	entry *centry;
 	struct BK_node **children;
 };
+
+
+/////////// functions that help with implementation ///////////
+
+// creates a BK_tree/index and initialize with no root and the Matchtype of the words
+static index* create_index(MatchType type) {
+	index* indx = malloc(sizeof(index));
+	indx->root = NULL;
+	indx->match_type = type;
+	return indx;
+}
 
 // creates a BK_node and initialized with an entry and no children
 static BK_node* create_BK_node(entry* entr) {
@@ -31,22 +46,25 @@ static int find_distance_words(word a, word b) {
 }
 */
 
-enum error_code build_entry_index(const entry_ist* el, enum match_type type, index* indx){
+////////// functions of BK_tree.h //////////////
+
+
+enum error_code build_entry_index(const entry_list* el, enum match_type type, index* indx){
 	unsigned int el_count = get_number_entries(el);		// get size of entry list
 	if (el_count <= 0) {		// etries do not exists
-		printf("Error in build_entry_index: Input entry_ist is empty\n");
+		printf("Error in build_entry_index: Input entry_list is empty\n");
 		return 1;
 	}
 
 	enum error_code err;
 	entry* entr = get_first(el);	// get first entry
 
-	indx = malloc(sizeof(index));		// create the BK_tree
-	indx->root = create_BK_node(entr);	// create node of root
+	indx = create_index(type);	// create the BK_tree
+	indx->root = create_BK_node(entr);		// create node of root
 
 	for(unsigned int i = 1; i < el_count; i++){
 		entr = get_next(el, entr);		
-		err = BK_insert_entry(entr, indx, MT_HAMMING_DIST);
+		err = BK_insert_entry(entr, indx, type);
 		if(err != EC_SUCCESS ) return err;
 	}
 }
@@ -56,7 +74,7 @@ enum error_code build_entry_index(const entry_ist* el, enum match_type type, ind
 // Απο δω και κάτω δεν έχω κοιτάξει ακόμα
 //////////////////////////////////////////////////
 
-enum error_code BK_insert_entry(entry *input, BK_node *tree,enum match_type type){
+enum error_code BK_insert_entry(entry *input, BK_node *tree, enum match_type type){
 	unsigned int dist = 0;
 
 	if(tree == NULL){
